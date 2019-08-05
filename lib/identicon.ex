34bbb758e3a7be416b7fb8619b0a53cc2ddf4e50
Kevin Szeto <kszeto@bleacherreport.com>
2019-08-05 14:10:38 -0700

    hash function and pick color

---
+++ lib/identicon.ex
@@ -0,0 +1,43 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  @doc """
+  Takes input string, generates a list of 16 numbers
+
+  ## Examples:
+    Identicon.hash_input("banana")
+    %Identicon.Image { hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65] }
+  """
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+end
---
+++ lib/image.ex
@@ -0,0 +1,9 @@
+defmodule Identicon.Image do
+  @moduledoc """
+  Struct for our Identicon image.
+  Struct enforces that the only properties are the ones we define in defstruct,
+  whereas a map allows you to have any property.
+  """
+
+  defstruct hex: nil, color: nil
+end
